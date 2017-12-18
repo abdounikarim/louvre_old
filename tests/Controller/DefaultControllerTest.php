9d13c0b1f487f59ee234bd13af7f1c41dee3b49f
casrime <casrime@macbook-pro-de-casrime.home>
2017-12-18 20:17:51 +0100

    adding .htaccess file, louvre.conf and .gitlab-ci.yml

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(0, 1);
+    }
+}
