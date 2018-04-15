c923c8922b448cb617b681ee05f62a7d3e5c1105
Karim <abdounikarim@gmail.com>
2018-04-15 22:44:42 +0200

    add form in functional test form homePage

--- src/Validator/Constraints/CheckDateValidator.php
+++ src/Validator/Constraints/CheckDateValidator.php
@@ -16,27 +16,29 @@ class CheckDateValidator extends ConstraintValidator
         $dayFormat = $value->format('D');
         $dayAndMonthFormat = $value->format('d/m');
 
-        // Musée fermé le mardi
-        if($dayFormat === 'Tue'){
-            $this->context->buildViolation("Le musée est fermé le mardi")
-                ->addViolation();
-        }
-
         // Musée fermé le 1er mai
-        else if($dayAndMonthFormat === '01/05'){
+        if($dayAndMonthFormat === '01/05'){
             $this->context->buildViolation("Le musée est fermé le 1er mai")
                 ->addViolation();
         }
-
         // Musée fermé le 1er novembre
         else if($dayAndMonthFormat === '01/11'){
             $this->context->buildViolation("Le musée est fermé le 1er novembre")
                 ->addViolation();
         }
-
         // Musée fermé le 25 décembre
         else if($dayAndMonthFormat === '25/12'){
             $this->context->buildViolation("Le musée est fermé le 25 décembre")
+                ->addViolation();
+        }
+        // Musée fermé le mardi
+        else if($dayFormat === 'Tue'){
+            $this->context->buildViolation("Le musée est fermé le mardi")
+                ->addViolation();
+        }
+        //Musée fermé le dimanche
+        else if($dayFormat === 'Sun'){
+            $this->context->buildViolation("Le musée est fermé le dimanche")
                 ->addViolation();
         }
 
--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Booking;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
@@ -10,8 +11,18 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/');
-
+        $crawler = $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $form = $crawler->selectButton('Valider')->form();
+        $booking = new Booking();
+        $booking->setVisitDay($form['booking[visitDay]'] = '16/04/2018');
+        $booking->setTicketNumber($form['booking[ticketNumber]'] = 1);
+        $booking->setTicketType($form['booking[ticketType]'] = "J");
+        $booking->setMail($form['booking[mail]'] = "abdounikarim@gmail.com");
+        $client->submit($form);
+
+        //echo $client->getResponse()->getContent();die();
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 }
